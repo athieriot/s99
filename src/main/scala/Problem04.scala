e3c32bd61b7e954d443575a531c231e48ceda283
Aurélien Thieriot <a.thieriot@gmail.com>
2011-06-03 18:24:50 +0200

    problem 4 solved

--- src/main/scala/Problem04.scala
+++ src/main/scala/Problem04.scala
@@ -1,11 +1,14 @@
 package scala
 
-import java.security.InvalidParameterException
-
 class Problem04 {
 
   def length[A](list: List[A]): Int = {
+    countRecursive(0, list)
+  }
 
-    return 6
+  private def countRecursive[A](actual: Int, list: List[A]): Int = (actual, list) match {
+    case (n, list :: Nil) => n + 1
+    case (n, list :: element) => countRecursive(n + 1, element)
+    case (n, _) => 0
   }
 }
